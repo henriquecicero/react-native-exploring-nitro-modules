e21664601b2ed599205ffffd3348eaa9fc3cbbb6
Henrique Cícero <henriquecicero@gmail.com>
2026-01-11 14:33:06 -0300

    refactor: remove older files of benchmark example

--- Logbook.nitro.ts
+++
@@ -1,9 +0,0 @@
-import { HybridObject } from 'react-native-nitro-modules';
-
-export interface Logbook extends HybridObject<{ android: 'c++'; ios: 'c++' }> {
-  getSamplesSize(input: string): number;
-  getSamplesCount(input: string): number;
-  getConcatString(input: string): string;
-  getConcatStringZeroCopy(input: string): ArrayBuffer;
-  add(a: number, b: number): number;
-}
