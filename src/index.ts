2973ac3776556e14c15466a3d91f029d9d215abb
Henrique Cícero <henriquecicero@gmail.com>
2026-01-19 22:21:08 -0300

    feat: export logbook interface

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,20 @@ export interface Math extends HybridObject<{
   add(a: number, b: number): number
 }
 
+export interface Logbook extends HybridObject<{
+  android: 'c++'
+  ios: 'swift'
+}> {
+  getSamplesSize(input: string): number
+  getSamplesCount(input: string): number
+  getConcatString(input: string): string
+  getConcatStringZeroCopy(input: string): ArrayBuffer
+}
+
 export function getMath(): Math {
   return NitroModules.createHybridObject<Math>('Math')
+}
+
+export function getLogbook(): Logbook {
+  return NitroModules.createHybridObject<Logbook>('Logbook')
 }
