4b8ee89be8dc7b7e718d0aa6bcddc0cd5e7dbe2e
Henrique Cícero <henriquecicero@gmail.com>
2026-01-19 22:19:53 -0300

    feat: add Logbook nitro config file

---
+++ src/specs/Logbook.nitro.ts
@@ -0,0 +1,11 @@
+import { type HybridObject } from 'react-native-nitro-modules'
+
+export interface Logbook extends HybridObject<{
+  android: 'c++'
+  ios: 'c++'
+}> {
+  getSamplesSize(input: string): number
+  getSamplesCount(input: string): number
+  getConcatString(input: string): string
+  getConcatStringZeroCopy(input: string): ArrayBuffer
+}
